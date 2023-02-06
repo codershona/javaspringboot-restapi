1e6b5fcc045fb70bc3402e10bd9ec3521b4b24c5
codershona <falgunimst95@gmail.com>
2023-02-06 18:12:36 +0000

    initial commit: New Spring Boot Project with Spring Initializr 1

---
+++ src/main/java/com/myjavaapi/myjavarestapi/MyjavarestapiApplication.java
@@ -0,0 +1,15 @@
+//This file has the main method of java New Spring Boot Project with Spring Initializr
+
+package com.myjavaapi.myjavarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjavarestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjavarestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myjavaapi/myjavarestapi/MyjavarestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjavaapi.myjavarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjavarestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
